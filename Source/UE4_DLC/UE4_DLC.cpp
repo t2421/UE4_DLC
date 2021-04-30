1afcabc025de575f6967116fcfd508681912a1e4
Taiga Kiyotaki <taiga@t2421.com>
2021-05-01 05:35:08 +0900

    init

---
+++ Source/UE4_DLC/UE4_DLC.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE4_DLC.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4_DLC, "UE4_DLC" );
---
+++ Source/UE4_DLC/UE4_DLCGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4_DLCGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_DLC_API AUE4_DLCGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
